f667371c2d01f62878b5f9aeb047de4573ba6a49
Gary Sheppard <gsheppard@esri.com>
2013-11-25 16:16:32 -0500

    Added PositionReportController

---
+++ source/MilitaryAppsLibrary/src/com/esri/militaryapps/controller/PositionReportController.java
@@ -0,0 +1,245 @@
+/*******************************************************************************
+ * Copyright 2013 Esri
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ ******************************************************************************/
+package com.esri.militaryapps.controller;
+
+import com.esri.militaryapps.model.Location;
+import com.esri.militaryapps.util.Utilities;
+import java.io.StringWriter;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * A controller that broadcasts position reports including current location.
+ */
+public class PositionReportController implements LocationListener {
+    
+    public static final int DEFAULT_PERIOD = 1000;
+    
+    private static final Logger logger = Logger.getLogger(PositionReportController.class.getName());
+    private static final String WKID_WGS1984 = "4326";
+    
+    private final OutboundMessageController outboundMessageController;
+    private final Object lastLocationLock = new Object();
+    private final Timer periodTimer = new Timer(true);
+
+    private boolean enabled = false;
+    private int period = DEFAULT_PERIOD;
+    private Location lastLocation = null;
+    private String username = null;
+    private String vehicleType = null;
+    private String uniqueId = null;
+    private TimerTask periodTimerTask = null;
+    
+    /**
+     * Instantiates a PositionReportController, which will start sending position
+     * reports after setEnabled(true) has been called and a location is available.
+     * @param locationController the LocationController that provides positions.
+     * @param outboundMessageController the transmitter of position report messages.
+     *                                  Other objects may use this controller at
+     *                                  the same time.
+     */
+    public PositionReportController(
+            LocationController locationController,
+            OutboundMessageController outboundMessageController,
+            String username,
+            String vehicleType,
+            String uniqueId) {
+        this.outboundMessageController = outboundMessageController;
+        this.username = username;
+        this.vehicleType = vehicleType;
+        this.uniqueId = uniqueId;
+        
+        locationController.addListener(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        synchronized (lastLocationLock) {
+            boolean startTimer = null == lastLocation;
+            lastLocation = location;
+            if (startTimer && enabled) {
+                startTimer();
+            }
+        }
+    }
+    
+    private void startTimer() {
+        if (null != periodTimerTask) {
+            periodTimerTask.cancel();
+        }
+        periodTimerTask = new TimerTask() {
+
+            @Override
+            public void run() {
+                sendPositionReport();
+            }
+        };
+        periodTimer.scheduleAtFixedRate(periodTimerTask, 0, period);
+    }
+
+    private void sendPositionReport() {
+        synchronized (lastLocationLock) {
+            if (null != lastLocation) {
+                try {
+                    DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                    Document doc = docBuilder.newDocument();
+                    Element geomessagesElement = doc.createElement("geomessages");
+                    doc.appendChild(geomessagesElement);
+                    Element geomessageElement = doc.createElement("geomessage");
+                    geomessageElement.setAttribute("version", "1.0");
+                    geomessagesElement.appendChild(geomessageElement);        
+
+                    Utilities.addTextElement(doc, geomessageElement,
+                            outboundMessageController.getTypePropertyName(), "position_report");
+                    Utilities.addTextElement(doc, geomessageElement,
+                            outboundMessageController.getIdPropertyName(), uniqueId);
+                    Utilities.addTextElement(doc, geomessageElement,
+                            outboundMessageController.getWkidPropertyName(), WKID_WGS1984);
+                    Utilities.addTextElement(doc, geomessageElement,
+                            outboundMessageController.getControlPointsPropertyName(), lastLocation.getLongitude() + "," + lastLocation.getLatitude());
+                    Utilities.addTextElement(doc, geomessageElement,
+                            outboundMessageController.getActionPropertyName(), "UPDATE");
+                    Utilities.addTextElement(doc, geomessageElement, "uniquedesignation", username);
+                    String dateString = Utilities.DATE_FORMAT_GEOMESSAGE.format(new Date());
+                    Utilities.addTextElement(doc, geomessageElement, "datetimesubmitted", dateString);
+                    Utilities.addTextElement(doc, geomessageElement, "datetimevalid", Utilities.DATE_FORMAT_GEOMESSAGE.format(lastLocation.getTimestamp().getTime()));
+
+                    StringWriter xmlStringWriter = new StringWriter();
+                    TransformerFactory.newInstance().newTransformer().transform(
+                            new DOMSource(doc), new StreamResult(xmlStringWriter));
+                    String messageText = xmlStringWriter.toString();
+                    outboundMessageController.sendMessage(messageText.getBytes());
+                } catch (Throwable t) {
+                    logger.log(Level.SEVERE, "Could not send chem light", t);
+                } finally {
+                    lastLocation = null;
+                }
+            }
+        }
+    }
+    
+    /**
+     * Sets whether this controller should send position reports or not. If currently
+     * disabled, enabling the controller immediately starts sending position reports
+     * when a location is available.
+     * @param enabled true if this controller should send position reports.
+     */
+    public void setEnabled(boolean enabled) {
+        boolean changed = this.enabled != enabled;
+        this.enabled = enabled;
+        if (enabled && changed) {
+            startTimer();
+        }
+    }
+
+    /**
+     * @return true if the controller is sending position reports.
+     */
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    /**
+     * @return the minimum number of milliseconds between position reports.
+     */
+    public int getPeriod() {
+        return period;
+    }
+
+    /**
+     * Sets the minimum number of milliseconds between position reports. The default
+     * is DEFAULT_PERIOD; setting this to a non-positive number results in setting the period
+     * to DEFAULT_PERIOD. Setting this to a different value than is currently set immediately
+     * starts sending position reports when a location is available.
+     * @param period the minimum number of milliseconds between position reports.
+     */
+    public void setPeriod(int period) {
+        if (period < 0) {
+            period = DEFAULT_PERIOD;
+        }
+        boolean changed = this.period != period;
+        this.period = period;
+        if (enabled && changed) {
+            startTimer();
+        }
+    }
+
+    /**
+     * @return the username used in position reports.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username used in position reports. The username ought
+     *                 to be human-readable and unique.
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the vehicle type used in position reports.
+     */
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    /**
+     * @param vehicleType the vehicle type used in position reports.
+     */
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    /**
+     * @return the unique ID for the vehicle. This ID should be different from that
+     *         of all other vehicles.
+     */
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    /**
+     * @param uniqueId the unique ID for the vehicle. This ID should be different
+     *                 from that of all other vehicles. One way to accomplish this
+     *                 is to use UUID.randomUUID().toString() to generate a unique
+     *                 ID for a vehicle that does not have one.
+     */
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+    
+    /**
+     * Returns the OutboundMessageController used by this controller.
+     * @return the OutboundMessageController used by this controller.
+     */
+    public OutboundMessageController getOutboundMessageController() {
+        return outboundMessageController;
+    }
+    
+}
---
+++ source/MilitaryAppsLibrary/test/com/esri/militaryapps/controller/test/PositionReportControllerTest.java
@@ -0,0 +1,200 @@
+/*******************************************************************************
+ * Copyright 2013 Esri
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ ******************************************************************************/
+package com.esri.militaryapps.controller.test;
+
+import com.esri.militaryapps.controller.LocationController;
+import com.esri.militaryapps.controller.LocationController.LocationMode;
+import com.esri.militaryapps.controller.OutboundMessageController;
+import com.esri.militaryapps.controller.PositionReportController;
+import com.esri.militaryapps.model.LocationProvider;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.xml.sax.SAXException;
+
+public class PositionReportControllerTest {
+    
+    private class Result {
+        String message = null;
+    }
+    
+    private static final int PORT = 48965;
+    private static final String USERNAME = "Honey Badgers 42G";
+    private static final String VEHICLE_TYPE = "HMMWV";
+    private static final String UID = UUID.randomUUID().toString();
+    private static final String PROPNAME_TYPE = "type";
+    private static final String PROPNAME_ID = "id";
+    private static final String PROPNAME_WKID = "wkid";
+    private static final String PROPNAME_CONTROL_POINTS = "control_points";
+    private static final String PROPNAME_ACTION = "action";
+    
+    private final PositionReportController controller;
+    
+    public PositionReportControllerTest() throws ParserConfigurationException, SAXException, IOException {
+        LocationController locController = new LocationController(LocationMode.SIMULATOR, false) {
+
+            @Override
+            protected LocationProvider createLocationServiceProvider() {
+                return null;
+            }
+        };
+        locController.start();
+        OutboundMessageController messageController = new OutboundMessageController(PORT) {
+            @Override
+            public String getTypePropertyName() {
+                return PROPNAME_TYPE;
+            }
+            
+            @Override
+            public String getIdPropertyName() {
+                return PROPNAME_ID;
+            }
+            
+            @Override
+            public String getWkidPropertyName() {
+                return PROPNAME_WKID;
+            }
+            
+            @Override
+            public String getControlPointsPropertyName() {
+                return PROPNAME_CONTROL_POINTS;
+            }
+            
+            @Override
+            public String getActionPropertyName() {
+                return PROPNAME_ACTION;
+            }
+        };
+        controller = new PositionReportController(locController, messageController, USERNAME, VEHICLE_TYPE, UID);
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testEnabled() {
+        System.out.println("enabled");
+        assertFalse(controller.isEnabled());
+        controller.setEnabled(true);
+        assertTrue(controller.isEnabled());
+        controller.setEnabled(false);
+        assertFalse(controller.isEnabled());
+    }
+
+    @Test
+    public void testPeriod() {
+        System.out.println("period");
+        assertEquals(PositionReportController.DEFAULT_PERIOD, controller.getPeriod());
+        controller.setPeriod(3456);
+        assertEquals(3456, controller.getPeriod());
+        controller.setPeriod(-42);
+        assertEquals(PositionReportController.DEFAULT_PERIOD, controller.getPeriod());
+        controller.setPeriod(PositionReportController.DEFAULT_PERIOD);
+        assertEquals(PositionReportController.DEFAULT_PERIOD, controller.getPeriod());
+    }
+    
+    @Test
+    public void testUsername() {
+        System.out.println("username");
+        assertEquals(USERNAME, controller.getUsername());
+        controller.setUsername("Woody");
+        assertEquals("Woody", controller.getUsername());
+        controller.setUsername(USERNAME);
+        assertEquals(USERNAME, controller.getUsername());
+    }
+    
+    @Test
+    public void testVehicleType() {
+        System.out.println("vehicleType");
+        assertEquals(VEHICLE_TYPE, controller.getVehicleType());
+        controller.setVehicleType("SR-71");
+        assertEquals("SR-71", controller.getVehicleType());
+        controller.setVehicleType(VEHICLE_TYPE);
+        assertEquals(VEHICLE_TYPE, controller.getVehicleType());
+    }
+    
+    @Test
+    public void testUniqueId() {
+        System.out.println("uniqueId");
+        assertEquals(UID, controller.getUniqueId());
+        controller.setUniqueId("Percy");
+        assertEquals("Percy", controller.getUniqueId());
+        controller.setUniqueId(UID);
+        assertEquals(UID, controller.getUniqueId());
+    }
+    
+    @Test
+    public void testMessage() throws Exception {
+        final Result result = new Result();
+        new Thread() {
+
+            @Override
+            public void run() {
+                byte[] message = new byte[1500];
+                DatagramPacket packet = new DatagramPacket(message, message.length);
+                DatagramSocket socket;
+                try {
+                    socket = new DatagramSocket(PORT);
+                    socket.receive(packet);
+                    String msgString = new String(packet.getData(), packet.getOffset(), packet.getLength());
+                    synchronized (result) {
+                        result.message = msgString;
+                    }
+                } catch (Throwable t) {
+                    Logger.getLogger(OutboundMessageControllerTest.class.getName()).log(Level.SEVERE, null, t);
+                }
+            }
+            
+        }.start();
+        
+        controller.getOutboundMessageController().setPort(PORT);
+        controller.setEnabled(true);
+        Thread.sleep(2000);
+        synchronized (result) {
+            /**
+             * Here we just make sure the message is not null and that it contains
+             * the username. Someone could add code to do a more rigorous test, realizing
+             * that elements won't be in any particular order in the XML message.
+             */
+            assertNotNull(result.message);
+            assertTrue(0 < result.message.indexOf(">" + USERNAME + "<"));
+        }
+    }
+    
+}
