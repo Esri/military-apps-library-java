92186444da5248770ae6f263c3ff50e38b8dd9d4
Gary Sheppard <gsheppard@esri.com>
2014-12-26 15:40:33 -0500

    Fixed threading issue in GeomessagesReader

--- source/MilitaryAppsLibrary/src/com/esri/militaryapps/model/GeomessagesReader.java
+++ source/MilitaryAppsLibrary/src/com/esri/militaryapps/model/GeomessagesReader.java
@@ -19,28 +19,19 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.ArrayList;
 import java.util.List;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 /**
  * A class that knows how to read geomessages and return Geomessage objects.
  */
-public class GeomessagesReader extends DefaultHandler {
+public class GeomessagesReader {
     
     private final SAXParser saxParser;
-
-    private boolean readingId = false;
-    private String elementName = null;
-    private final ArrayList<Geomessage> messages = new ArrayList<Geomessage>();
-    private Geomessage message = null;
-    private String version = null;
 
     /**
      * Instantiates a GeomessagesReader that can be used multiple times.
@@ -52,40 +43,6 @@ public class GeomessagesReader extends DefaultHandler {
         saxParser = SAXParserFactory.newInstance().newSAXParser();
     }
     
-    @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        if ("message".equalsIgnoreCase(qName) || "geomessage".equalsIgnoreCase(qName)) {
-            message = new Geomessage();
-            messages.add(message);
-            version = attributes.getValue("v");
-        } else if (Geomessage.ID_FIELD_NAME.equalsIgnoreCase(qName)) {
-            readingId = true;
-        }
-        elementName = qName;
-    }
-
-    @Override
-    public void characters(char[] ch, int start, int length) throws SAXException {
-        String charString = new String(ch, start, length);
-        if (null != message) {
-            if (readingId) {
-                message.setId(charString);
-            } else if (null != elementName) {
-                message.setProperty(elementName, charString);
-            }
-        }
-    }
-
-    @Override
-    public void endElement(String uri, String localName, String qName) throws SAXException {
-        if (Geomessage.ID_FIELD_NAME.equalsIgnoreCase(qName)) {
-            readingId = false;
-        } else if ("message".equalsIgnoreCase(qName) || "geomessage".equalsIgnoreCase(qName)) {
-            message = null;
-        }
-        elementName = null;
-    }
-
     /**
      * Parses an XML file of messages and returns a list of messages.
      * @param xmlMessageFile the XML message file.
@@ -94,12 +51,11 @@ public class GeomessagesReader extends DefaultHandler {
      * @throws SAXException
      */
     public List<Geomessage> parseMessages(File xmlMessageFile) throws IOException, SAXException {
+        GeomessagesHandler handler = new GeomessagesHandler();
         synchronized (saxParser) {
-            messages.clear();
-            message = null;
-            saxParser.parse(new FileInputStream(xmlMessageFile), this);
-            return messages;
+            saxParser.parse(new FileInputStream(xmlMessageFile), handler);
         }
+        return handler.getGeomessages();
     }
 
     /**
@@ -110,12 +66,11 @@ public class GeomessagesReader extends DefaultHandler {
      * @throws SAXException
      */
     public List<Geomessage> parseMessages(String xmlMessages) throws IOException, SAXException {
+        GeomessagesHandler handler = new GeomessagesHandler();
         synchronized (saxParser) {
-            messages.clear();
-            message = null;
-            saxParser.parse(new InputSource(new StringReader(xmlMessages)), this);
-            return messages;
+            saxParser.parse(new InputSource(new StringReader(xmlMessages)), handler);
         }
+        return handler.getGeomessages();
     }
     
 }
