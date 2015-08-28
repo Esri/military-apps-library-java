310a57205d1db49d4d49d70335315daebed6ade6
Gary Sheppard <gsheppard@esri.com>
2015-08-28 16:43:40 -0400

    Added Mil2525CMessageParser
    A lot of this is copied from Vehicle Commander. We will refactor Vehicle
    Commander shortly and also add the parser to Squad Leader.

---
+++ source/MilitaryAppsLibrary/src/com/esri/militaryapps/model/Mil2525CMessageParser.java
@@ -0,0 +1,135 @@
+/*******************************************************************************
+ * Copyright 2015 Esri
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ ******************************************************************************/
+package com.esri.militaryapps.model;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * A parser for MIL-STD-2525C messages in XML. The easiest thing to do is to call
+ * parseMessages. But you can also use it as a handler with a SAXParser if desired.<br/>
+ * <br/>
+ * The resulting messages are implementation-specific, so the implementing developer
+ * should provide a way to retrieve the parsed messages.
+ */
+public abstract class Mil2525CMessageParser extends DefaultHandler {
+    private final SAXParser saxParser;
+
+    private boolean readingId = false;
+    private String elementName = null;
+    private String version = null;
+
+    /**
+     * Creates a new Mil2525CMessageParser.
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     */
+    public Mil2525CMessageParser() throws ParserConfigurationException, SAXException {
+        saxParser = SAXParserFactory.newInstance().newSAXParser();
+    }
+    
+    /**
+     * Called when a new message is encountered. Implementations should create a
+     * new message object and add it to a list of messages that have been parsed.
+     */
+    protected abstract void newMessage();
+    
+    /**
+     * Sets the message ID for the message currently being parsed.
+     * @param id the message ID for the message currently being parsed.
+     */
+    protected abstract void setMessageId(String id);
+    
+    /**
+     * Sets a property value for the message currently being parsed.
+     * @param key the property name.
+     * @param value the property value.
+     */
+    protected abstract void setMessageProperty(String key, String value);
+    
+    /**
+     * Returns the message ID property name for your implementation. For example,
+     * in ArcGIS Runtime, return MessageHelper.MESSAGE_ID_PROPERTY_NAME.
+     * @return the message ID property name for your implementation.
+     */
+    protected abstract String getMessageIdPropertyName();
+    
+    /**
+     * Removes the messages that were parsed by this parser.
+     */
+    public abstract void clearMessages();
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        if ("message".equals(qName) || "geomessage".equals(qName)) {
+            newMessage();
+            version = attributes.getValue("v");
+        } else if (getMessageIdPropertyName().equals(qName)) {
+            readingId = true;
+        }
+        elementName = qName;
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        String charString = new String(ch, start, length);
+        if (readingId) {
+            setMessageId(charString);
+        } else {
+            setMessageProperty(elementName, charString);
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (getMessageIdPropertyName().equals(qName)) {
+            readingId = false;
+        }
+        elementName = null;
+    }
+
+    /**
+     * Parses an XML file of messages.
+     * @param xmlMessageFile the XML message file.
+     * @throws IOException
+     * @throws SAXException
+     */
+    public synchronized void parseMessages(File xmlMessageFile) throws IOException, SAXException {
+        clearMessages();
+        saxParser.parse(new FileInputStream(xmlMessageFile), this);
+    }
+
+    /**
+     * Parses an XML string of messages.
+     * @param xmlMessages  the XML message string.
+     * @throws IOException
+     * @throws SAXException
+     */
+    public synchronized void parseMessages(String xmlMessages) throws IOException, SAXException {
+        clearMessages();
+        saxParser.parse(new InputSource(new StringReader(xmlMessages)), this);
+    }
+
+}
---
+++ source/MilitaryAppsLibrary/test/com/esri/militaryapps/model/Mil2525CMessageParserTest.java
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ * Copyright 2015 Esri
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ ******************************************************************************/
+package com.esri.militaryapps.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.xml.sax.SAXException;
+
+public class Mil2525CMessageParserTest {
+    
+    public Mil2525CMessageParserTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of parseMessages method, of class Mil2525CMessageParser.
+     */
+    @Test
+    public void testParseMessages_String() throws Exception {
+        System.out.println("parseMessages");
+        String type0 = "position_report";
+        String id0 = "12345678";
+        String type1 = "spotrep";
+        String id1 = "12345679";
+        String xmlMessages = "<geomessages><geomessage v=\"1.1\"><_type>" + type0 + "</_type><id>" + id0 + "</id></geomessage><geomessage v=\"1.1\"><id>" + id1 + "</id><_type>" + type1 + "</_type></geomessage></geomessages>";
+        Mil2525CMessageParserImpl instance = new Mil2525CMessageParserImpl();
+        instance.parseMessages(xmlMessages);
+        assertEquals(2, instance.messages.size());
+        HashMap message0 = instance.messages.get(0);
+        assertEquals(type0, message0.get("_type"));
+        assertEquals(id0, message0.get("id"));
+        HashMap message1 = instance.messages.get(1);
+        assertEquals(type1, message1.get("_type"));
+        assertEquals(id1, message1.get("id"));
+        instance.clearMessages();
+        assertEquals(0, instance.messages.size());
+    }
+
+    public class Mil2525CMessageParserImpl extends Mil2525CMessageParser {
+        
+        public static final String ID_FIELD_NAME = "id";
+        public HashMap<String, String> message = null;
+        public List<HashMap> messages = new ArrayList<HashMap>();
+
+        public Mil2525CMessageParserImpl() throws ParserConfigurationException, SAXException {
+            super();
+        }
+
+        public void newMessage() {
+            message = new HashMap<String, String>();
+            messages.add(message);
+        }
+
+        public void setMessageId(String id) {
+            message.put(ID_FIELD_NAME, id);
+        }
+
+        public void setMessageProperty(String key, String value) {
+            message.put(key, value);
+        }
+
+        public String getMessageIdPropertyName() {
+            return ID_FIELD_NAME;
+        }
+
+        public void clearMessages() {
+            messages.clear();
+        }
+    }
+}
